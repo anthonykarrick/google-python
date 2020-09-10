92dba852157a77bc1df5a11a0b0fb9dc8a95c741
akarrick <38015875+anthonykarrick@users.noreply.github.com>
2020-09-09 21:40:04 -0700

    programming-with-python5-coursera-google-python_reusingcode

---
+++ programming-with-python5-coursera-google-python.py
@@ -0,0 +1,10 @@
+#this was written in a google lesson because there were 2 lines or repeated code, now you could put in any name and 
+#dont really gotta write a lot more code, lesson was reusing code
+
+
+def lucky_number(name):
+    number = len(name) * 9
+    print("hello " + name _ ". Your lucky number is " + str(number))
+
+lucky_number("Kay")
+lucky_number("Cameron")
