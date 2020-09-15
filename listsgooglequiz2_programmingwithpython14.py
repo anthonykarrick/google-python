a6494af74576fd2ef7cf6ff4411fabae3466c3fe
akarrick <38015875+anthonykarrick@users.noreply.github.com>
2020-09-14 21:19:16 -0700

    listsgooglequiz2_programmingwithpython14

---
+++ listsgooglequiz2_programmingwithpython14.py
@@ -0,0 +1,14 @@
+#had to write a function that translates to pig latin in a list
+
+def pig_latin(text):
+  words = text.split()
+  pigged_text = []
+
+  for word in words:
+    word = word[1:] + word[0] + 'ay'
+    pigged_text.append(word)
+
+  return ' '.join(pigged_text)
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
