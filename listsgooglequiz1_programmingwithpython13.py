a2156030aa50546110f99146d170391892b88792
akarrick <38015875+anthonykarrick@users.noreply.github.com>
2020-09-14 21:15:35 -0700

    listsgooglequiz1_programmingwithpython13

---
+++ listsgooglequiz1_programmingwithpython13.py
@@ -0,0 +1,15 @@
+#lists quiz from google course
+
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+newfilenames = []
+for names in filenames:
+    if names.endswith('.hpp'):
+        newfilenames.append(names[:-2])
+        continue
+    newfilenames.append(names)
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
